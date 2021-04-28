f608d2ac8689c6a167b247f8f9b0017fb338a810
Gabriel Rodrigues <33073778+gabrielroodriz@users.noreply.github.com>
2021-04-28 00:27:04 -0300

    fix: Development of ExerperienceBar Component and Home

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+export function ExperienceBar() {
+  return (
+    <header className={styles.experienceBar}>
+      <span>xp</span>
+      <div>
+        <div style={{ width: "60%" }} />
+        <span className={styles.currentExperience} style={{ left: "50%" }}>
+          330 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
