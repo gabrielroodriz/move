cb8faa07a6f0ab49a91e28a643726291869de0ac
Gabriel Rodrigues <33073778+gabrielroodriz@users.noreply.github.com>
2021-05-08 00:29:21 -0300

    Creation Countdown context and adding notification with sound

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,20 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
-      <img src="https://www.hdnit.com.br/assinatura/foto-gabrielsouza.jpg" alt="Gabriel" />
+      <img
+        src="https://www.hdnit.com.br/assinatura/foto-gabrielsouza.jpg"
+        alt="Gabriel"
+      />
       <div>
         <strong>Gabriel Rodrigues</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-          Lavel 3
+          Level {level}
         </p>
       </div>
     </div>
